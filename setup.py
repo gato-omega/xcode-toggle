7ee2d94510b90cdc9060044b3cf75e3138782208
Chris Patrick Schreiner <chris.patrick.schreiner@gmail.com>
2016-02-11 10:45:33 +0100

    Now depends on python3. Added default behaviour (--list). Also added version information (--version). v0.0.3

--- setup.py
+++ setup.py
@@ -1,18 +1,27 @@
+import re
 from setuptools import setup
+
+VERSIONFILE = "_version.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
 
 setup(
     name='xcode-toggle',
-    version='0.0.2',
+    version=verstr,
     url='https://github.com/schwa/xcode-toggle',
     license='MIT',
     author='Jonathan Wight',
     author_email='schwa@schwa.io',
     description='A better "xcode-select"',
-    py_modules=['xcode_toggle'],
-    install_requires=['click', 'pathlib2', 'blessings'],
+    py_modules=['xcode_toggle',"_version"],
+    install_requires=['click', 'pathlib', 'blessings'],
     entry_points='''
         [console_scripts]
         xcode-toggle=xcode_toggle:main
     ''',
 )
-
--- xcode_toggle.py
+++ xcode_toggle.py
@@ -1,19 +1,26 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 
 import subprocess
 import os
-import sys
-
-from pathlib2 import Path
+from pathlib import Path
 from blessings import Terminal
 import click
+from _version import __version__
 
 __author__ = 'schwa'
 
 term = Terminal()
 
+def print_version(ctx, param, value):
+    if not value or ctx.resilient_parsing:
+        return
+    click.echo('{}, version {}'.format("xcode-toggle", __version__))
+    ctx.exit()
+
 
 @click.command()
+@click.option('--version', '-v', is_flag=True, callback=print_version,
+              expose_value=False, is_eager=True, help="Show version information")
 @click.option("-p", "--print", "command", flag_value="print", help="Print current select Xcode path")
 @click.option("-l", "--list", "command", flag_value="list", help="List all found Xcode paths")
 @click.option("-t", "--toggle", "command", flag_value="toggle", help="Toggle between all Xcode paths")
@@ -22,16 +29,21 @@ term = Terminal()
 @click.option("-x", "--toolchains", "command", flag_value="toolchains", help="Select Swift toolchains interactively")
 @click.argument("path", nargs=-1)
 def main(command, path):
+    """
+    Tool for fast switching between Xcode versions.
+
+    The '--list' command is the default behaviour.
+    """
     if command == "print":
-        print get_current_path()
-    elif command == "list":
+        click.echo(get_current_path())
+    elif command in [None, "list"]:
         list_paths()
     elif command == "toggle":
         paths = get_paths()
         current_path = get_current_path()
         index = paths.index(current_path)
         next_path = paths[(index + 1) % len(paths)]
-        print "Switching to: {t.bold}{path}{t.normal}".format(t=term, path=next_path)
+        click.echo("Switching to: {t.bold}{path}{t.normal}".format(t=term, path=next_path))
         subprocess.check_call(["/usr/bin/sudo", "/usr/bin/xcode-select", "-s", str(next_path)])
         list_paths()
     elif command == "ask":
@@ -39,49 +51,37 @@ def main(command, path):
         xcode_paths = get_paths()
         for index, path in enumerate(xcode_paths):
             if current_path == path:
-                print "[{index}] {path} {t.bold}[Current]{t.normal}".format(t=term, index=index, path=path)
+                click.echo("[{index}] {path} {t.bold}[Current]{t.normal}".format(t=term, index=index, path=path))
             else:
-                print "[{index}] {path}".format(t=term, index=index, path=path)
+                click.echo("[{index}] {path}".format(t=term, index=index, path=path))
         option = input("Enter a number: ")
         option = int(option)
         next_path = xcode_paths[option]
         subprocess.check_call(["/usr/bin/sudo", "/usr/bin/xcode-select", "-s", str(next_path)])
         list_paths()
     elif command == "toolchains":
-
-#$ export PATH=/Library/Developer/Toolchains/swift-latest.xctoolchain/usr/bin:"${PATH}"
-
+        # $ export PATH=/Library/Developer/Toolchains/swift-latest.xctoolchain/usr/bin:"${PATH}"
         shell_path = Path(os.environ["SHELL"])
-
-        # path = "foobar"
-
         toolchains_path = Path("/Library/Developer/Toolchains")
-        paths = list(toolchains_path .glob("*"))
+        paths = list(toolchains_path.glob("*"))
         path = choose(paths)
-        print "Using {}".format(path)
-        os.environ["PATH"] = os.environ["PATH"] + ":{}/usr/bin".format(path)
+        click.echo("Using {}".format(path))
+        os.environ["PATH"] += ":{}/usr/bin".format(path)
         os.system(str(shell_path))
-
-
-        #     if shell == "fish":
-        #         print 'set -x PATH {}/usr/bin "${{PATH}}"'.format(path)
-        #     else:
-        #         print 'export PATH={}/usr/bin:"${{PATH}}"'.format(path)
 
     else:
         raise Exception("Unknown command: ", command)
 
 
-def choose(items, current = None):
+def choose(items, current=None):
     for index, item in enumerate(items):
         if current == item:
-            print "[{index}] {item} {t.bold}[Current]{t.normal}".format(t=term, index=index, item=item)
+            click.echo("[{index}] {item} {t.bold}[Current]{t.normal}".format(t=term, index=index, item=item))
         else:
-            print "[{index}] {item}".format(t=term, index=index, item=item)
+            click.echo("[{index}] {item}".format(t=term, index=index, item=item))
     option = input("Enter a number: ")
     option = int(option)
     return items[option]
-
 
 
 def list_paths():
@@ -89,20 +89,21 @@ def list_paths():
     xcode_paths = get_paths()
     for path in xcode_paths:
         if current_path == path:
-            print "{t.bold}{path} {t.red}[Current]{t.normal}".format(t=term, path=path)
+            click.echo("{path} {t.bold}[Current]{t.normal}".format(t=term, path=path))
         else:
-            print "{path}".format(t=term, path=path)
+            click.echo("{path}".format(t=term, path=path))
 
 
 def get_paths():
-    output = subprocess.check_output(["/usr/bin/mdfind", 'kMDItemCFBundleIdentifier="com.apple.dt.Xcode" and kMDItemContentType="com.apple.application-bundle"'])
+    output = subprocess.check_output(["/usr/bin/mdfind",
+                                      'kMDItemCFBundleIdentifier="com.apple.dt.Xcode" and kMDItemContentType="com.apple.application-bundle"'])
     output = output.strip()
-    paths = [Path(path) for path in output.split("\n")]
+    paths = [Path(path) for path in output.decode("utf-8").split("\n")]
     return sorted(paths)
 
 
 def get_current_path():
-    current_path = Path(subprocess.check_output(["/usr/bin/xcode-select", "-p"]))
+    current_path = Path(subprocess.check_output(["/usr/bin/xcode-select", "-p"]).decode("utf-8"))
     for index, part in enumerate(current_path.parts):
         if Path(part).suffix == ".app":
             return Path(*current_path.parts[:index + 1])
